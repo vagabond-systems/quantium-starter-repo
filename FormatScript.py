19090749fea5c1314151a9d54ccae0a0b20a1810
Alaa <alaa.ismail@student.guc.edu.eg>
2022-09-24 16:57:35 +0200

    Added Option to filter by region & Enhancement in Design

--- FormatScript.py
+++ FormatScript.py
@@ -1,29 +1,25 @@
 import pandas as pd
 import os
 
-# Merge the 3 csv
-# Make a new column sales before removing quantity & price & product
-# Then write it to a new formated csv
-
 def readCSV(path):
-    df = pd.read_csv(path, delimiter=";", parse_dates=['date'], dayfirst=True)
-    for x in df.index:
-        if df.loc[x, 'product'] != "pink morsel":
-            df.drop(x, inplace=True)
+    df_i = pd.read_csv(path, delimiter=";", parse_dates=['date'], dayfirst=True)
 
-    df['sales'] = pd.Series(df['price'].str[1:]).astype(float) * df['quantity'].astype(int)
+    df_i = df_i[df_i['product'] == "pink morsel"]
 
-    df.drop("price", axis=1, inplace=True)
-    df.drop("quantity", axis=1, inplace=True)
-    df.drop("product", axis=1, inplace=True)
+    # Make a new column sales before removing quantity & price & product
+    df_i['sales'] = pd.Series(df_i['price'].str[1:]).astype(float) * df_i['quantity'].astype(int)
 
-    return df
+    df_i.drop("price", axis=1, inplace=True)
+    df_i.drop("quantity", axis=1, inplace=True)
+    df_i.drop("product", axis=1, inplace=True)
 
+    return df_i
 
+# Merge the 3 csv
 filepaths = ["./data/" + f for f in os.listdir("./data/") if f.endswith('.csv')]
-
 df = pd.concat(map(readCSV, filepaths))
 
+# Then write it to a new formated csv
 df.to_csv('formatted_data.csv', index=False)
 
 print(df)
--- app.py
+++ app.py
@@ -1,17 +1,52 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("formatted_data.csv", parse_dates=['date'], dayfirst=True)
-df['date']= df['date'].dt.to_period('M')
+DATA_PATH = "./formatted_data.csv"
+COLORS = {
+    "primary": "#31C6D4",
+    "secondary": "#FF1E1E",
+    "font": "#522A61"
+}
+
+df = pd.read_csv(DATA_PATH, parse_dates=['date'], dayfirst=True)
+df['date'] = df['date'].dt.to_period('M')
 df = df.groupby([df['date'].astype(str), 'region'])['sales'].sum()
 
 app = Dash(__name__)
-fig = px.line(df, x= df.index.get_level_values(0), y = 'sales',  color = df.index.get_level_values(1))
+
 app.layout = html.Div([
-    dcc.Graph(id='Sales', figure=fig
-    )
-])
+    html.H1("Pink Morsel Visualizer", style={
+        "background-color": COLORS["secondary"],
+        "color": COLORS["font"],
+        "border-radius": "20px" }),
+
+    dcc.Graph(id='SalesGraph'),
+
+    html.Div(dcc.RadioItems(['all', 'north', 'south', 'east', 'west'], 'all', id='RadioItem', inline=True),
+             style = {"font-size": "150%"})
+
+], style={'width': '100%',"textAlign": "center",
+        'display': 'inline-block', "background-color": COLORS["primary"],
+        "border-radius": "20px"})
+
+
+@app.callback(
+    Output('SalesGraph', 'figure'),
+    Input('RadioItem', 'value')
+)
+def update_graph(radio_item):
+    if radio_item == 'all':
+        dff = df
+    else:
+        dff = df[df.index.get_level_values('region').isin([radio_item])]
+
+    fig = px.line(dff, x=dff.index.get_level_values(0), y='sales', color=dff.index.get_level_values('region'))
+    fig.update_xaxes(title='Date')
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
