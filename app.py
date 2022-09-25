e47ef6dac44daa521f63acf9d8e681e53c1c43b4
Alaa <alaa.ismail@student.guc.edu.eg>
2022-09-25 07:36:51 +0200

    Added tests

--- app.py
+++ app.py
@@ -16,25 +16,26 @@ df = df.groupby([df['date'].astype(str), 'region'])['sales'].sum()
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("Pink Morsel Visualizer", style={
+    html.H1(["Pink Morsel Visualizer"], id = 'header', style={
         "background-color": COLORS["secondary"],
         "color": COLORS["font"],
-        "border-radius": "20px" }),
+        "border-radius": "20px"}),
 
-    dcc.Graph(id='SalesGraph'),
+    html.Div(dcc.Graph(id='SalesGraph'),
+             style={}),
 
     html.Div(dcc.RadioItems(['all', 'north', 'south', 'east', 'west'], 'all', id='RadioItem', inline=True),
-             style = {"font-size": "150%"})
+             style={"font-size": "150%"})
 
-], style={'width': '100%',"textAlign": "center",
-        'display': 'inline-block', "background-color": COLORS["primary"],
-        "border-radius": "20px"})
+
+], style={'width': '100%', "textAlign": "center",
+          'display': 'inline-block', "background-color": COLORS["primary"],
+          "border-radius": "20px"})
 
 
 @app.callback(
     Output('SalesGraph', 'figure'),
-    Input('RadioItem', 'value')
-)
+    Input('RadioItem', 'value'))
 def update_graph(radio_item):
     if radio_item == 'all':
         dff = df
@@ -43,7 +44,7 @@ def update_graph(radio_item):
 
     fig = px.line(dff, x=dff.index.get_level_values(0), y='sales', color=dff.index.get_level_values('region'))
     fig.update_xaxes(title='Date')
-    fig.update_layout(transition_duration=500)
+    fig.update_layout(transition_duration=1000)
 
     return fig
 
