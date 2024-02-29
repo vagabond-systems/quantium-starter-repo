d47b5ddeddd3e9aca19fe29eed218742859cd499
Jasper <jasper.gp.robbins@gmail.com>
2024-02-29 12:16:33 +1100

    add tests for component layout

---
+++ test_components.py
@@ -0,0 +1,17 @@
+import chromedriver_autoinstaller
+from task_three import app
+
+chromedriver_autoinstaller.install()
+
+
+def test_header_is_present(dash_duo):
+    dash_duo.start_server(app)
+    assert dash_duo.wait_for_element_by_id("Header", timeout=2)
+
+def test_region_picker_is_present(dash_duo):
+    dash_duo.start_server(app)
+    assert dash_duo.wait_for_element_by_id("RadioButton", timeout=2)
+
+def test_visualisation_is_present(dash_duo):
+    dash_duo.start_server(app)
+    assert dash_duo.wait_for_element_by_id("ProductSalesGraph", timeout=2)
